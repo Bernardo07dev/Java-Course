4212076a36f2507b3275be05d3b6f930f7c954b4
Bernardo <bernardomoreira.dev@gmail.com>
2026-07-17 12:54:51 -0300

    .

--- InitialProject/Adding.java
+++ InitialProject/Adding.java
@@ -1,3 +1,4 @@
+package InitialProject;
 import java.util.Scanner;
 
 public class Adding{
---
+++ InitialProject/IsNegative.java
@@ -0,0 +1,8 @@
+package InitialProject;
+
+public class IsNegative {
+      public static void main(String[] args){
+            System.out.println("Hello");
+      }
+      
+}
